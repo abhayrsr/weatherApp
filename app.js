d152d2a709e4afcb8a157e4e5346068b54e00e2b
abhay <abhayrajsinghrana123@gmail.com>
2023-06-20 13:05:15 +0530

    feat: api

---
+++ app.js
@@ -0,0 +1,4 @@
+const API_KEY = '57b9e94bf0807a16a282b0784abecce1';
+
+const API = 'https://api.openweathermap.org/data/2.5/{API_ENDPOINT}?{PARAMETERS}&appid={YOUR_API_KEY}';
+const IMG_URL = 'http://openweathermap.org/img/wn/';
