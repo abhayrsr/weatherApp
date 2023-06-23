f9113d0937a7506ff3095c0a6677b8f87358e657
abhay <abhayrajsinghrana123@gmail.com>
2023-06-23 18:29:51 +0530

    refactor: form

--- app.js
+++ app.js
@@ -1,4 +1,20 @@
 const API_KEY = '57b9e94bf0807a16a282b0784abecce1';
-
-const API = 'https://api.openweathermap.org/data/2.5/{API_ENDPOINT}?{PARAMETERS}&appid={YOUR_API_KEY}';
+const form = document.querySelector('form');
+const search = document.querySelector('#search');
+const weather = document.querySelector('#weather');
+//const API = 'https://api.openweathermap.org/data/2.5/{API_ENDPOINT}?{PARAMETERS}&appid={YOUR_API_KEY}';
 const IMG_URL = 'http://openweathermap.org/img/wn/';
+
+const getWeather = async(city) => {
+    const url = 'https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric';
+    const response = await fetch(url);
+    console.log(response);
+}
+
+form.addEventListener(
+    "submit",
+    function(event){
+        console.log(search.value)
+        event.preventDefault();
+    }
+)
